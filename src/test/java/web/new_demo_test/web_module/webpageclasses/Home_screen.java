bce9dd3887cba212888a30ad9c1284b3b840c587
root <santhosh.annam@devrabbit.com>
2019-09-09 10:54:31 +0530

    commit

--- src/test/java/web/new_demo_test/web_module/webpageclasses/Home_screen.java
+++ src/test/java/web/new_demo_test/web_module/webpageclasses/Home_screen.java
@@ -10,7 +10,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 @SuppressWarnings("unused")
 public class Home_screen extends BaseClass {
 	
-	public static String primaryInfo  = "{\"user_id\":85,\"executed_user_id\":85,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"new_demo_test\",\"project_description\":\"desc\",\"project_id\":406,\"module_name\":\"web_module\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":676,\"testcase_name\":\"tc\",\"testcase_id\":570,\"testset_id\":0,\"executed_timestamp\":342652443,\"browser_type\":\"chrome\",\"testcase_overwrite\":true,\"client_timezone_id\":\"Asia/Calcutta\"}";
+	public static String primaryInfo  = "{\"user_id\":85,\"executed_user_id\":85,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"new_demo_test\",\"project_description\":\"desc\",\"project_id\":406,\"module_name\":\"web_module\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":676,\"testcase_name\":\"TC1rterter\",\"testcase_id\":571,\"testset_id\":0,\"executed_timestamp\":343605771,\"browser_type\":\"chrome\",\"testcase_overwrite\":true,\"client_timezone_id\":\"Asia/Calcutta\"}";
 
 	public static String projectName = "new_demo_test";
 	public WebDriver driver;
